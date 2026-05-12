c4cda7178d45c3667807f0114e6f06a9d523af2f
alexisczl <kzal.alexis@gmail.com>
2026-05-12 09:15:40 +0200

    Add activation and regression loss functions

--- src/losses.py
+++ src/losses.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class MSE:
     @staticmethod
     def loss(y_true, y_pred):
@@ -6,9 +8,30 @@ class MSE:
         :param y_pred: (array) Prediction vector
         :return: (flt)
         """
-        ### WRITE YOUR CODE HERE
+        return np.mean((y_true - y_pred) ** 2)
 
     @staticmethod
     def gradient(y_true, y_pred):
-        ### WRITE YOUR CODE HERE
-        return
+        return 2 * (y_pred - y_true) / y_true.shape[0]
+
+class RMSE:
+    @staticmethod
+    def loss(y_true, y_pred):
+        return np.sqrt(np.mean((y_true - y_pred) ** 2))
+    
+    @staticmethod
+    def gradient(y_true, y_pred):
+        eps = 1e-12
+        mse = np.mean((y_true - y_pred) ** 2)
+        return (y_pred - y_true) / (y_true.shape[0] * np.sqrt(mse + eps))
+    
+class MAE: 
+    @staticmethod
+    def loss(y_true, y_pred):
+        return np.mean(np.abs(y_pred - y_true))
+
+    @staticmethod
+    def gradient(y_true, y_pred):
+        return np.sign(y_pred - y_true) / y_true.shape[0]
+    
+
